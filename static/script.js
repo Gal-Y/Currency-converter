bd873faa0cd615c3ffee6c6753836c91a67bc00c
Galen Yuan <galenYuan@student.unsw.edu.au>
2024-05-23 16:31:07 +1000

    added script.js

---
+++ static/script.js
@@ -0,0 +1,29 @@
+$(document).ready(function() {
+    const currencyDataElement = $('#currency-data');
+    const currencyList = JSON.parse(currencyDataElement.attr('data-currencies'));
+
+    const baseCurrencySelect = $('#base_currency');
+    const targetCurrencySelect = $('#target_currency');
+
+    // Populate dropdowns with currencies
+    currencyList.forEach(currency => {
+        baseCurrencySelect.append(new Option(currency, currency));
+        targetCurrencySelect.append(new Option(currency, currency));
+    });
+
+    $('#convert-form').on('submit', function(event) {
+        event.preventDefault();
+        $.ajax({
+            url: '/convert',
+            method: 'POST',
+            data: $(this).serialize(),
+            success: function(response) {
+                if (response.success) {
+                    $('#result').text('Converted Amount: ' + response.converted_amount);
+                } else {
+                    $('#result').text('Error: ' + response.error);
+                }
+            }
+        });
+    });
+});
